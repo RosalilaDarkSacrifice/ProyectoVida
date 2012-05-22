45e0af6420970b53a42b6029513231f29893e66d
Ahmed Castro <antares_43@hotmail.com>
2012-05-22 12:23:02 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoVida::Application.config.secret_token = 'ca9a7c5d25e0a133acb416101f42ca5bfca8a9b4c980ff2b372d3d406bf1e0bfca4de85115059e52a3d11bbebd2459b1c44ac147cd366c65e6ce455af407b904'
